0cb61057ebbe53ba05f19d98c03088509dbf85bd
David MH Ryu <myeonghan12@gmail.com>
2020-03-24 14:53:00 +1000

    read/write, stringify/toString/parse

---
+++ playground/1-json.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+// const book = {
+// 	title: "Harry Potter",
+// 	author: "JKR"
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync("1-JSON.json", bookJSON);
+
+// const dataBuffer = fs.readFileSync("1-JSON.json");
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+
+// console.log(data.title);
