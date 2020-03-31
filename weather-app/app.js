1a102c5c6e79a4b6c3e7dae2df52a36a27d1194e
David MH Ryu <myeonghan12@gmail.com>
2020-03-31 18:05:32 +1000

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,27 +7,36 @@ const mapBoxUrl =
 	"https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibXlkYXYxMiIsImEiOiJjazhmazV4bXYwNTZrM21vemV1NHhpYTh5In0._yL2n_UfOxYh1X6hvzQylw&limit=1";
 
 request({ url: weatherUrl, json: true }, (error, response) => {
-	// console.log(response.body.currently);
-
 	const summary = response.body.daily.data[0].summary;
 	const temperature = response.body.currently.temperature;
 	const precipProbability = response.body.currently.precipProbability;
 
-	console.log(
-		summary +
-			" It is currently " +
-			temperature +
-			" degrees out. There is a " +
-			precipProbability +
-			"% chance of rain."
-	);
+	if (error) {
+		console.log("Unable to connect to weather service!");
+	} else if (response.body.error) {
+		console.log("Unable to find location");
+	} else {
+		console.log(
+			summary +
+				" It is currently " +
+				temperature +
+				" degrees out. There is a " +
+				precipProbability +
+				"% chance of rain."
+		);
+	}
 });
 
 // Geocoding
 // Address -> Lat/Long -> Weather
 request({ url: mapBoxUrl, json: true }, (error, response) => {
-	const latitude = response.body.features[0].center[1];
-	const longitude = response.body.features[0].center[0];
-
-	console.log(latitude, longitude);
+	if (error) {
+		console.log("Unable to connect to location services.");
+	} else if (response.body.features.length === 0) {
+		console.log("Unable to find location");
+	} else {
+		const latitude = response.body.features[0].center[1]; // This should be inside of else statement to make use of the variables.
+		const longitude = response.body.features[0].center[0];
+		console.log(latitude, longitude);
+	}
 });
